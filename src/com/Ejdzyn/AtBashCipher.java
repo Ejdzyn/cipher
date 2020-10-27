379b78b81bb8841b3e57f5d709f3f79b42ccf15c
Adrian <adrian.wstepnik@gmail.com>
2020-10-27 14:10:45 +0100

    final

--- src/com/Ejdzyn/AtBashCipher.java
+++ src/com/Ejdzyn/AtBashCipher.java
@@ -3,23 +3,21 @@ package com.Ejdzyn;
 public class AtBashCipher implements Cipher{
 
     private final char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    private final String ab = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
 
     @Override
     public String decode(String message) {
-        return null;
-    }
-
-    @Override
-    public String encode(String message) {
         StringBuilder encodedPass = new StringBuilder();
-
         for(int i = 0 ; i < message.length();i++){
             char checked = message.charAt(i);
             if(Character.isAlphabetic(checked)){
                 for(int y = 0 ; y < alphabet.length;y++){
-                    if(alphabet[y]==checked){
+                    if(checked==alphabet[y]){
                         encodedPass.append(alphabet[alphabet.length-1-y]);
+                        break;
+                    }else if(checked==Character.toLowerCase(alphabet[y])){
+                        encodedPass.append(Character.toLowerCase(alphabet[alphabet.length-1-y]));
                         break;
                     }
                 }
@@ -27,7 +25,28 @@ public class AtBashCipher implements Cipher{
                 encodedPass.append(checked);
             }
         }
+        return String.valueOf(encodedPass);
+    }
 
+    @Override
+    public String encode(String message) {
+        StringBuilder encodedPass = new StringBuilder();
+        for(int i = 0 ; i < message.length();i++){
+            char checked = message.charAt(i);
+            if(Character.isAlphabetic(checked)){
+                for(int y = 0 ; y < alphabet.length;y++){
+                    if(checked==alphabet[y]){
+                        encodedPass.append(alphabet[alphabet.length-1-y]);
+                        break;
+                    }else if(checked==Character.toLowerCase(alphabet[y])){
+                        encodedPass.append(Character.toLowerCase(alphabet[alphabet.length-1-y]));
+                        break;
+                    }
+                }
+            } else {
+                encodedPass.append(checked);
+            }
+        }
         return String.valueOf(encodedPass);
     }
 }
